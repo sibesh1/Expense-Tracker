de66b42f2005b05bd4631952774966171e732f86
sibesh1 <sibesh1998@gmail.com>
2020-12-31 15:42:00 +0530

    added responsive design for mobiles

--- src/App.js
+++ src/App.js
@@ -17,13 +17,16 @@ function App() {
         style={{ height: "95vh", marginBottom: "4rem" }}
         className={styles.grid}
       >
-        <Grid item lg={4} sm={3} xs={12}>
+        <Grid item lg={4} sm={3} xs={12} className={styles.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item lg={3} sm={4} xs={12}>
+        <Grid item lg={3} sm={4} xs={12} className={styles.main}>
           <Main />
         </Grid>
-        <Grid item lg={4} sm={3} xs={12}>
+        <Grid item lg={4} sm={3} xs={12} className={styles.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item lg={4} sm={3} xs={12} className={styles.last}>
           <Details title="Expense" />
         </Grid>
       </Grid>
